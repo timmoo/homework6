a5499c53b9c79e39265abf9ed35591a16111146a
timmoo <47295295+timmoo@users.noreply.github.com>
2019-03-19 09:02:21 +0300

    Update SQLQuery6-1.sql

--- SQLQuery6-1.sql
+++ SQLQuery6-1.sql
@@ -1,10 +1,11 @@
-
 WITH TotalSummCTE AS 
 	(SELECT MONTH(INV.InvoiceDate) AS Mesyac1, 
 	YEAR(INV.InvoiceDate) as God1, 
 	SUM(INVL.Quantity*INVL.UnitPrice) AS TotalSumm
 	FROM Sales.InvoiceLines AS INVL
-	JOIN Sales.Invoices AS INV ON INV.InvoiceID=INVL.InvoiceID AND INV.InvoiceID<=INVL.InvoiceID  WHERE INV.InvoiceDate >= '2015-01-01'
+	JOIN Sales.Invoices AS INV 
+	ON INV.InvoiceID=INVL.InvoiceID AND INV.InvoiceID<=INVL.InvoiceID  
+	WHERE INV.InvoiceDate >= '2015-01-01'
 	GROUP BY MONTH(INV.InvoiceDate), YEAR(INV.InvoiceDate)
 	)
 SELECT DISTINCT
@@ -12,12 +13,15 @@ I.InvoiceID,
 C.CustomerName,
 I.InvoiceDate, 
 SUM(S.Quantity*S.UnitPrice) AS Summ,
-SUM(TCTE.TotalSumm)
+TCTE.TotalSumm AS Total
 FROM Sales.InvoiceLines AS S 
-JOIN Sales.Invoices AS I ON I.InvoiceID=S.InvoiceID
-JOIN Sales.Customers AS C ON I.CustomerID=C.CustomerID
-JOIN TotalSummCTE AS TCTE ON TCTE.Mesyac1=MONTH(I.InvoiceDate) and TCTE.God1=YEAR(I.InvoiceDate)
-WHERE I.InvoiceDate >= '2015-01-01'
+JOIN Sales.Invoices AS I 
+ON I.InvoiceID=S.InvoiceID
+JOIN Sales.Customers AS C 
+ON I.CustomerID=C.CustomerID
+JOIN TotalSummCTE AS TCTE 
+ON TCTE.Mesyac1=MONTH(I.InvoiceDate) and TCTE.God1=YEAR(I.InvoiceDate)
+WHERE I.InvoiceDate >= '2015-01-01' 
 GROUP BY  I.InvoiceID, C.CustomerName, I.InvoiceDate, TCTE.TotalSumm
 ORDER BY I.InvoiceID;
 
@@ -28,8 +32,10 @@ C.CustomerName,
 S.InvoiceDate, 
 SUM(I.Quantity*I.UnitPrice) OVER (ORDER BY YEAR(S.InvoiceDate), MONTH(S.InvoiceDate) RANGE UNBOUNDED PRECEDING) AS TotalSumm  
 FROM Sales.Invoices AS S
-JOIN Sales.InvoiceLines AS I ON S.InvoiceID=I.InvoiceID
-JOIN Sales.Customers AS C ON S.CustomerID=C.CustomerID
+JOIN Sales.InvoiceLines AS I 
+ON S.InvoiceID=I.InvoiceID
+JOIN Sales.Customers AS C 
+ON S.CustomerID=C.CustomerID
 WHERE S.InvoiceDate >= '2015-01-01'
 GROUP BY  I.InvoiceID, C.CustomerName, S.InvoiceDate, I.Quantity, I.UnitPrice
 )
