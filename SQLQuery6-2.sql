574bec247dad97d4fba4865c68b6089577ee1677
timmoo <47295295+timmoo@users.noreply.github.com>
2019-03-20 15:02:35 +0300

    Update SQLQuery6-2.sql

--- SQLQuery6-2.sql
+++ SQLQuery6-2.sql
@@ -1,25 +1,12 @@
-﻿/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT DISTINCT MAX(IL.Quantity) OVER(PARTITION BY MONTH(I.InvoiceDate)), W.StockItemName, IL.Quantity
-  FROM Sales.InvoiceLines AS IL
-  JOIN Sales.Invoices AS I
-  ON IL.InvoiceID=I.InvoiceID
-   JOIN Warehouse.StockItems AS W
-  ON IL.StockItemID=W.StockItemID
-
-  Where I.InvoiceDate>='2016-01-01' AND  I.InvoiceDate<='2016-12-31'
-  ORDER BY IL.Quantity desc,  W.StockItemName desc;
-
 WITH CTE AS 
-	(SELECT ROW_NUMBER()  OVER(PARTITION BY MONTH(I.InvoiceDate)ORDER BY IL.Quantity) AS NUMBER, W.StockItemName, IL.Quantity, W.StockItemID 
-  FROM Sales.InvoiceLines AS IL
-  JOIN Sales.Invoices AS I
-  ON IL.InvoiceID=I.InvoiceID
-   JOIN Warehouse.StockItems AS W
-  ON IL.StockItemID=W.StockItemID
-  Where I.InvoiceDate>='2016-01-01' AND  I.InvoiceDate<='2016-12-31'
-  )
-  Select * from Sales.InvoiceLines AS IL
- JOIN CTE AS CTE_ 
- ON IL.StockItemID=CTE_.StockItemID
- where CTE_.NUMBER<=2
- ORDER BY IL.Quantity desc;
+	( SELECT IL.*,W.StockItemID, W.StockItemName, I.*, ROW_NUMBER()  OVER(PARTITION BY MONTH(I.InvoiceDate), YEAR(I.InvoiceDate)ORDER BY IL.Quantity desc) AS NUMBER FROM Sales.InvoiceLines  AS IL 
+INNER JOIN Sales.Invoices AS I
+ON IL.InvoiceID=I.InvoiceID
+INNER JOIN Warehouse.StockItems AS W
+ON IL.StockItemID=W.StockItemID
+WHERE I.InvoiceDate>='2016-01-01' AND  I.InvoiceDate<='2016-12-31'
+)
+ Select CTE_.* FROM Sales.InvoiceLines AS IL
+  INNER JOIN CTE AS CTE_
+  ON IL.InvoiceID=CTE_.InvoiceID
+   where CTE_.NUMBER<=2;
