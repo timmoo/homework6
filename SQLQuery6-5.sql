81a2ed8b45d70ab7b0f3b9380121f4cac8048828
timmoo <47295295+timmoo@users.noreply.github.com>
2019-03-21 20:41:47 +0300

    Update SQLQuery6-5.sql

--- SQLQuery6-5.sql
+++ SQLQuery6-5.sql
@@ -1,21 +1,21 @@
 
 WITH CTE AS 
 	( 
-		SELECT C.CustomerID, 
+	SELECT C.CustomerID, 
 		C.CustomerName,  
 		IL1.UnitPrice AS [UnitPrice],  
 		IL1.StockItemID AS StockID, 
 		I.InvoiceDate AS [DATE], 
-	ROW_NUMBER()  OVER(PARTITION BY I.CustomerID ORDER BY IL1.UnitPrice desc) AS NUMBER FROM Sales.InvoiceLines  AS IL1
-INNER JOIN Sales.Invoices AS I ON IL1.InvoiceID=I.InvoiceID
-INNER JOIN Sales.Customers AS C ON I.CustomerID=C.CustomerID
+		ROW_NUMBER()  OVER(PARTITION BY I.CustomerID ORDER BY IL1.UnitPrice desc) AS NUMBER FROM Sales.InvoiceLines  AS IL1
+		INNER JOIN Sales.Invoices AS I ON IL1.InvoiceID=I.InvoiceID
+		INNER JOIN Sales.Customers AS C ON I.CustomerID=C.CustomerID
 )
  Select
- DISTINCT CTE.CustomerID, 
- CTE.CustomerName, 
- CTE.StockID, 
- CTE.UnitPrice, 
- CTE.DATE, CTE.NUMBER FROM CTE 
+	 DISTINCT CTE.CustomerID, 
+ 	CTE.CustomerName, 
+ 	CTE.StockID, 
+ 	CTE.UnitPrice, 
+ 	CTE.DATE, CTE.NUMBER FROM CTE 
   where CTE.NUMBER<=2
-   ORDER BY CTE.CustomerID;
+  ORDER BY CTE.CustomerID;
  
