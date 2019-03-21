3e7cbf98e04e306104e07566d7032e8aabb14abd
timmoo <47295295+timmoo@users.noreply.github.com>
2019-03-21 20:33:32 +0300

    Update SQLQuery6-3.sql

--- SQLQuery6-3.sql
+++ SQLQuery6-3.sql
@@ -1,20 +1,16 @@
-  WITH CTE AS
-  (
-   SELECT *,COUNT(*) OVER (PARTITION BY (W.StockItemName) ORDER BY (W.StockItemName) RANGE UNBOUNDED PRECEDING) AS [COUNT_by NAME]
-   FROM Warehouse.StockItems  AS W 
-  )
- SELECT CTE.StockItemID,
-	CTE.StockItemName,
-	CTE.Brand,
-	CTE.UnitPrice, 
-	COUNT(*) OVER ( ORDER BY (CTE.StockItemID) RANGE UNBOUNDED PRECEDING) AS [COUNT_ALL],
-		ROW_NUMBER()  OVER (ORDER BY CTE.StockItemID desc) AS [NUMBER], CTE.[COUNT_by NAME],
-	LAG(CTE.StockItemID) OVER (ORDER BY (CTE.StockItemName) ) as PRevious,
-	LAG(CTE.StockItemID, 2) OVER (ORDER BY (CTE.StockItemName) ) as PRevious2,
-	LEAD(CTE.StockItemID) OVER (ORDER BY (CTE.StockItemName) ) as Follow,
-	NTILE(30)  OVER (ORDER BY CTE.TypicalWeightPerUnit desc) AS [weightgroup]
-	  FROM CTE
-	ORDER BY NUMBER, COUNT_ALL, CTE.StockItemID
-	
-	-- В запросе со смещением на 2 назад не удалось заменить NULL на NO Items с ошибкой данных не INT. Как исправить?
-	--Так же вопрос по выборке по имени с различной начальной букве CTE.[COUNT_by NAME], как сделать фильтр?
+ 
+ SELECT 
+	W.StockItemID,
+	W.StockItemName,
+	W.Brand,
+	W.UnitPrice, 
+	COUNT(*) OVER (PARTITION BY LEFT(W.StockItemName,1) ORDER BY (W.StockItemName) RANGE UNBOUNDED PRECEDING) AS [CountByName],
+	COUNT(*) OVER ( ORDER BY (W.StockItemName) RANGE UNBOUNDED PRECEDING) AS [CountAll],
+	ROW_NUMBER() OVER (PARTITION BY LEFT(W.StockItemName,1) ORDER BY W.StockItemName) AS [Number],
+	COUNT(*)OVER(PARTITION	BY LEFT(W.StockItemName,1)) AS [CountByName],
+	LEAD(W.StockItemID) OVER (ORDER BY (W.StockItemName) ) as [Follow],
+	LAG(W.StockItemID) OVER (ORDER BY (W.StockItemName) ) as [Previous],
+	LAG(W.StockItemName, 2, 'No Items') OVER (ORDER BY (W.StockItemName) ) as [Previous2],
+	NTILE(30)  OVER (ORDER BY W.TypicalWeightPerUnit desc) AS [WeightGroup]
+	FROM Warehouse.StockItems AS W
+	ORDER BY W.StockItemName;
