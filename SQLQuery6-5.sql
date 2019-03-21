7d7eec0827315b7adefd372bf2d0ca904130dc4a
timmoo <47295295+timmoo@users.noreply.github.com>
2019-03-21 19:42:33 +0300

    Update SQLQuery6-5.sql

--- SQLQuery6-5.sql
+++ SQLQuery6-5.sql
@@ -1,15 +1,24 @@
 
 WITH CTE AS 
-	( SELECT C.CustomerID, C.CustomerName,  IL1.UnitPrice AS [UnitPrice],  W.StockItemName AS StockNAME,w.StockItemID AS StockID, I.InvoiceDate AS [DATE], 
+	( 
+		SELECT C.CustomerID, 
+		C.CustomerName,  
+		IL1.UnitPrice AS [UnitPrice],  
+		W.StockItemName AS StockNAME,
+		w.StockItemID AS StockID, 
+		I.InvoiceDate AS [DATE], 
 	ROW_NUMBER()  OVER(PARTITION BY I.CustomerID ORDER BY IL1.UnitPrice desc) AS NUMBER FROM Sales.InvoiceLines  AS IL1
-INNER JOIN Sales.Invoices AS I
-ON IL1.InvoiceID=I.InvoiceID
-INNER JOIN Warehouse.StockItems AS W
-ON IL1.StockItemID=W.StockItemID
-INNER JOIN Sales.Customers AS C
-ON I.CustomerID=C.CustomerID
+INNER JOIN Sales.Invoices AS I ON IL1.InvoiceID=I.InvoiceID
+INNER JOIN Warehouse.StockItems AS W ON IL1.StockItemID=W.StockItemID
+INNER JOIN Sales.Customers AS C ON I.CustomerID=C.CustomerID
 )
- Select DISTINCT CTE.CustomerID, CTE.CustomerName, CTE.StockID, CTE.StockNAME, CTE.UnitPrice, CTE.DATE, CTE.NUMBER FROM CTE 
+ Select
+ DISTINCT CTE.CustomerID, 
+ CTE.CustomerName, 
+ CTE.StockID, 
+ CTE.StockNAME, 
+ CTE.UnitPrice, 
+ CTE.DATE, CTE.NUMBER FROM CTE 
   where CTE.NUMBER<=2
    ORDER BY CTE.CustomerID;
    
