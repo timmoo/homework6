23a1ea443b3685d602bc11257910bb8a3604e4fd
timmoo <47295295+timmoo@users.noreply.github.com>
2019-03-21 22:16:30 +0300

    Update SQLQuery6-4.sql

--- SQLQuery6-4.sql
+++ SQLQuery6-4.sql
@@ -11,3 +11,18 @@ FROM Sales.Invoices AS I
 	JOIN Sales.InvoiceLines AS IL ON IL.InvoiceID = I.InvoiceID
 GROUP BY P.FullName, I.CustomerID, C.CustomerName, I.SalespersonPersonID, I.InvoiceDate, IL.UnitPrice
 ORDER BY I.InvoiceDate DESC;
+
+
+SELECT 
+	distinct I.SalespersonPersonID, 
+	P.FullName, 
+	LAST_VALUE(C.CustomerName) OVER (PARTITION BY IL.InvoiceID ORDER BY I.InvoiceDate desc), 
+	LAST_VALUE(I.CustomerID) OVER (PARTITION BY IL.InvoiceID ORDER BY I.InvoiceDate desc), 
+	LAST_VALUE(IL.UnitPrice) OVER (PARTITION BY IL.InvoiceID ORDER BY I.InvoiceDate desc), 
+	LAST_VALUE(I.InvoiceDate) OVER (PARTITION BY IL.InvoiceID ORDER BY I.InvoiceDate desc)
+FROM Sales.Invoices AS I
+	JOIN Application.People AS P ON P.PersonID = I.SalespersonPersonID
+	JOIN Sales.Customers AS C ON C.CustomerID = I.CustomerID
+	JOIN Sales.InvoiceLines AS IL ON IL.InvoiceID = I.InvoiceID
+GROUP BY P.FullName, I.CustomerID, C.CustomerName, I.SalespersonPersonID, I.InvoiceDate, IL.UnitPrice
+ORDER BY P.FullName;
