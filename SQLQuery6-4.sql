9ca6c1209ef93a93a340288e4a912b7f7c366095
timmoo <47295295+timmoo@users.noreply.github.com>
2019-03-21 21:25:58 +0300

    Update SQLQuery6-4.sql

--- SQLQuery6-4.sql
+++ SQLQuery6-4.sql
@@ -1,23 +1,13 @@
- 
- 
- 
- SELECT  P.PersonID, P.FullName, I.CustomerID, C.CustomerName
-	  FROM Application.People AS P
-	 INNER JOIN Sales.Invoices AS I
-	 ON P.PersonID=I.SalespersonPersonID
-	 INNER JOIN Sales.Customers AS C
-	 ON C.CustomerID=I.CustomerID
-	WHERE I.CustomerID not  exists (select distinct (LAST_VALUE ( I.CustomerID) OVER (PARTITION BY P.PersonID 	ORDER BY P.PersonID 
-						ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)) AS ID, P.PersonID, P.FullName, I.CustomerID,C.CustomerName
-						 FROM Application.People AS P
-	 INNER JOIN Sales.Invoices AS I
-	 ON P.PersonID=I.SalespersonPersonID
-	 INNER JOIN Sales.Customers AS C
-	 ON C.CustomerID=I.CustomerID);
-
-						SELECT  P.PersonID, P.FullName, I.CustomerID,C.CustomerName
-	  FROM Application.People AS P
-	 INNER JOIN Sales.Invoices AS I
-	 ON P.PersonID=I.SalespersonPersonID
-	 INNER JOIN Sales.Customers AS C
-	 ON C.CustomerID=I.CustomerID;
+SELECT 
+	I.SalespersonPersonID AS [SalesPersonID], 
+	P.FullName AS [SalesPersonName], 
+	LAST_VALUE(C.CustomerName) OVER (PARTITION BY C.CustomerName ORDER BY C.CustomerName) AS [LastCustomerName], 
+	I.CustomerID, 
+	IL.UnitPrice AS [UnitPrice],
+	I.InvoiceDate AS [Date]
+FROM Sales.Invoices AS I
+	JOIN Application.People AS P ON P.PersonID = I.SalespersonPersonID
+	JOIN Sales.Customers AS C ON C.CustomerID = I.CustomerID
+	JOIN Sales.InvoiceLines AS IL ON IL.InvoiceID = I.InvoiceID
+GROUP BY P.FullName, I.CustomerID, C.CustomerName, I.SalespersonPersonID, I.InvoiceDate, IL.UnitPrice
+ORDER BY I.InvoiceDate DESC;
