5cc48bb53d0899e1aea8568094f3f347dd28410d
timmoo <47295295+timmoo@users.noreply.github.com>
2019-03-21 20:36:22 +0300

    Update SQLQuery6-1.sql

--- SQLQuery6-1.sql
+++ SQLQuery6-1.sql
@@ -6,10 +6,8 @@ C.CustomerName,
 IL.Quantity*IL.UnitPrice as [Sale Amount],
 (select sum(ILInner.Quantity*ILInner.UnitPrice)
 from [Sales].[InvoiceLines] as ILInner
-join [Sales].[Invoices] as IInner
-on IInner.InvoiceID = ILInner.InvoiceID
-where IInner.InvoiceDate >= '2015-01-01' 
-and IInner.InvoiceDate <= eomonth(I.InvoiceDate)
+join [Sales].[Invoices] as IInner on IInner.InvoiceID = ILInner.InvoiceID
+where IInner.InvoiceDate >= '2015-01-01' and IInner.InvoiceDate <= eomonth(I.InvoiceDate)
 ) as [Sale Amount by Month]
 from [Sales].[InvoiceLines] as IL
 join [Sales].[Invoices] as I on I.InvoiceID = IL.InvoiceID
@@ -22,14 +20,12 @@ I.InvoiceID,
 S.InvoiceDate, 
 C.CustomerName,
 I.Quantity*I.UnitPrice as [Sale Amount],
-SUM(I.Quantity*I.UnitPrice) OVER (ORDER BY YEAR(S.InvoiceDate), MONTH(S.InvoiceDate) RANGE UNBOUNDED PRECEDING) AS TotalSumm  
+SUM(I.Quantity*I.UnitPrice) OVER (ORDER BY YEAR(S.InvoiceDate), 
+MONTH(S.InvoiceDate) RANGE UNBOUNDED PRECEDING) AS TotalSumm  
 FROM Sales.Invoices AS S
-JOIN Sales.InvoiceLines AS I 
-ON S.InvoiceID=I.InvoiceID
-JOIN Sales.Customers AS C 
-ON S.CustomerID=C.CustomerID
-where S.InvoiceDate >= '2015-01-01' 
-and S.InvoiceDate <= eomonth(S.InvoiceDate)
+JOIN Sales.InvoiceLines AS I ON S.InvoiceID=I.InvoiceID
+JOIN Sales.Customers AS C ON S.CustomerID=C.CustomerID
+where S.InvoiceDate >= '2015-01-01' and S.InvoiceDate <= eomonth(S.InvoiceDate)
 order by S.InvoiceDate, [Sale Amount], I.InvoiceID, C.CustomerName;
 
 --SQL Server parse and compile time: 
